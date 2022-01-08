2b330133f1d888444809ca9efed8b11a44d03425
Willem Allan <willemarf@outlook.com>
2022-01-07 22:08:54 -0300

    Update bomb.py

--- bomb.py
+++ bomb.py
@@ -1,86 +1,92 @@
 import time
-from pynput.mouse import Controller, Button
+import mouse
 from pyautogui import press
 
 # segundos para voltar a trabalhar
-time_work = 60
+time_work = 3600
 
-connect_button_x = 950
-connect_button_y = 650
+def connect():
+    print('connecting')
+    connect_button_x = '950'
+    connect_button_y = '650'
 
-metamask_subcribe_x = 1800
-metamask_subcribe_y = 600
+    mouse.move(connect_button_x, connect_button_y, absolute=True, duration=0.1)
+    mouse.click('left')
 
-heroes_x = 1370
-heroes_y = 690
+    time.sleep(8)
 
-work_all_x = 880
-work_all_y = 330
+def metamask():
+    print('metamask')
+    metamask_subcribe_x = 1800
+    metamask_subcribe_y = 550
 
-work_back_x = 1015
-work_back_y = 283
-
-miner_x = 1015
-miner_y = 483
-
-mouse = Controller()
-
-def reset_position():
-    mouse.move(-10000, -10000)
-    time.sleep(0.2)
-
-def join():
-
-    # click connect
-    reset_position()
-    mouse.move(connect_button_x, connect_button_y)
-    time.sleep(0.5)
-    mouse.click(Button.left)
-
-    # waiting load metamask
-    time.sleep(10)
-
-    # subcribe metamask
-    reset_position()
-    mouse.move(metamask_subcribe_x, metamask_subcribe_y)
-    time.sleep(0.5)
-    mouse.click(Button.left)
+    mouse.move(metamask_subcribe_x, metamask_subcribe_y, absolute=True, duration=0.1)
+    mouse.click('left')
 
     # waiting load game
     time.sleep(15)
 
-    # select heroes
-    reset_position()
-    mouse.move(heroes_x, heroes_y)
-    time.sleep(0.5)
-    mouse.click(Button.left)
+def heroes():
+    print('selecting heroes')
+    heroes_x = 1370
+    heroes_y = 690
+
+    mouse.move(heroes_x, heroes_y, absolute=True, duration=0.1)
+    mouse.click('left')
 
     time.sleep(1)
+
+def work():
+    print('work all')
+    work_all_x = 880
+    work_all_y = 330
+
+    mouse.move(work_all_x, work_all_y, absolute=True, duration=0.1)
+    mouse.click('left')
+
+    time.sleep(1)
+
+def home():
+    print('backing home...')
+    work_back_x = 1015
+    work_back_y = 283
+
+    mouse.move(work_back_x, work_back_y, absolute=True, duration=0.1)
+    mouse.click('left')
+
+    time.sleep(1)
+
+def mining():
+    miner_x = 1015
+    miner_y = 483
+
+    mouse.move(miner_x, miner_y, absolute=True, duration=0.1)
+    mouse.click('left')
+    print('mining...')
+
+def rotine():
+    # CONNECTING GAME
+    connect()
+
+    # METAMASK
+    metamask()
+
+    # SELECTING HEROES
+    heroes()
 
     # ALL HEROES FOR WORK
-    reset_position()
-    mouse.move(work_all_x, work_all_y)
-    time.sleep(0.5)
-    mouse.click(Button.left)
+    work()
 
-    time.sleep(1)
+    # HOME
+    home()
 
-    # BACK HOME
-    reset_position()
-    mouse.move(work_back_x, work_back_y)
-    time.sleep(0.5)
-    mouse.click(Button.left)
-
-    time.sleep(1)
-
-    # INITIAL MINING BOMBERS
-    reset_position()
-    mouse.move(miner_x, miner_y)
-    time.sleep(0.5)
-    mouse.click(Button.left)
+    # MINING
+    mining()
 
 while True:
-    join()
-    time.sleep(time_work)
+    time.sleep(5)
     press('f5')
     time.sleep(10)
+    print('starting...')
+    rotine()
+    time.sleep(time_work)
