e495cca78fba3e20a779f39b5813ac1204eeb0d6
Willem Allan <willemarf@outlook.com>
2022-03-06 02:41:35 -0300

    Update bomb_linux.py

--- bomb_linux.py
+++ bomb_linux.py
@@ -4,68 +4,35 @@ import pyautogui
 # tempo trabalhando em segundos
 time_work = 3200
 
-def click_connect():
-    pyautogui.click(x=150, y=150)
-    time.sleep(5)
+def click_button(x, y, msg, second):
+    print(msg)
+    pyautogui.click(x=x, y=y)
+    time.sleep(second)
 
-def click_connect_metamask():
-    print('conectando sua carteira')
-    pyautogui.click(x=950, y=650)
-    time.sleep(20)
-
-def click_sign_metamask():
-    print('conectando sua carteira - metamask')
-    pyautogui.click(x=950, y=595)
-    time.sleep(20)
-
-def click_metamask():
-    print('solicitação de assinatura do bombcrypto')
-    pyautogui.click(x=1800, y=550)
-    time.sleep(20)
-
-def click_heroes():
-    print('entrando na listagem de bombers')
-    pyautogui.click(x=1370, y=690)
-    time.sleep(5)
-
-def click_work_all():
-    print('colocando todos os bombers para trabalhar')
-    pyautogui.click(x=870, y=330)
-    time.sleep(5)
-
-def click_close():
-    print('voltando para home')
-    pyautogui.click(x=1000, y=271)
-    time.sleep(5)
-
-def click_hunt():
-    print('iniciando a mineração')
-    pyautogui.click(x=962, y=450)
-
-def rotine():    
-    click_connect_metamask()
+def rotine():
+    click_button(705, 626, 'connect', 5)
     
-    click_sign_metamask()
+    click_button(691, 560, 'connect metamask', 30)
 
-    click_metamask()
+    click_button(1269, 593, 'sign metamask', 30)
 
-    click_heroes()
+    click_button(1094, 697, 'heroes', 5)
 
-    click_work_all()
+    click_button(599, 315, 'work all', 5)
 
-    click_close()
+    click_button(735, 265, 'close', 5)
 
-    click_hunt()
+    click_button(696, 442, 'hunt', 5)
 
 while True:
     print('deixe o navegador aberto no site do bombcrypto')
-    time.sleep(5)
-    click_connect()
+    time.sleep(2)
+    click_button(150, 150, 'selecting windows', 1)
 
     pyautogui.hotkey('ctrl', 'r')
-    print('aguardando 15 segundos para começar')
-    time.sleep(15)
+    print('aguardando 20 segundos para começar')
+    time.sleep(20)
     
-    print('iniciando...')
+    print('starting...')
     rotine()
     time.sleep(time_work)
