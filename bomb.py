20f5123d763163b7fd94928595a25cdbde7a5237
Willem Allan <willemarf@outlook.com>
2022-03-23 23:29:57 -0300

    Update bomb.py

--- bomb.py
+++ bomb.py
@@ -3,6 +3,8 @@ import datetime
 import mouse
 import keyboard
 
+# url do game
+# https://app.bombcrypto.io/webgl/
 
 # tempo trabalhando em segundos
 time_work = 4500
@@ -25,19 +27,19 @@ def refresh_page():
 def rotine():
     print("iniciando rotina")
 
-    click_button(950, 650, "abrindo o jogo", 5)
+    click_button(950, 750, "abrindo o jogo", 5)
 
-    click_button(950, 595, "conectando a metamask", 20)
+    click_button(950, 655, "conectando a metamask", 20)
 
-    click_button(1800, 550, "solicitação de assinatura do bombcrypto", 20)
+    click_button(1800, 570, "solicitação de assinatura do bombcrypto", 20)
 
-    click_button(1370, 690, "entrando na listagem de bombers", 5)
+    click_button(1370, 800, "entrando na listagem de bombers", 5)
 
-    click_button(880, 330, "colocando todos os bombers para trabalhar", 5)
+    click_button(880, 410, "colocando todos os bombers para trabalhar", 5)
 
-    click_button(1000, 271, "voltando para home", 5)
+    click_button(1010, 371, "voltando para home", 5)
 
-    click_button(1015, 383, "iniciando a mineração", 5)
+    click_button(1015, 523, "iniciando a mineração", 5)
 
 
 print("Deixe o navegador aberto no site do bombcrypto com a metamask logada")
