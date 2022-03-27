e89c5fa3df2eb2ca7218fb99a2944bbcec494bc8
Willem Allan <willemarf@outlook.com>
2022-03-26 22:36:00 -0300

    Update bomb_linux.py

--- bomb_linux.py
+++ bomb_linux.py
@@ -3,46 +3,36 @@ import datetime
 import pyautogui
 
 # tempo trabalhando em segundos
-time_work = 4500
-
+time_work = 5400
 
 def click_button(x, y, msg, second):
     print(msg)
     pyautogui.click(x=x, y=y)
     time.sleep(second)
 
-
-def refresh_page():
-    print("Clicando CTRL+R - recarregar a página")
-    pyautogui.hotkey("ctrl", "r")
-    print("Aguardando 15 segundos para começar")
-    time.sleep(15)
-
-
 def rotine():
-    print("Iniciando...")
+    click_button(964, 682, 'connect', 5)
+    
+    click_button(964, 550, 'login', 20)
 
-    click_button(964, 682, "carregando jogo", 5)
+    click_button(1363, 759, 'heroes', 5)
 
-    click_button(978, 618, "conectando na metamask", 20)
+    click_button(864, 374, 'work all', 5)
 
-    click_button(1821, 621, "assinando carteira", 20)
+    click_button(1005, 323, 'close', 5)
 
-    click_button(1363, 759, "selecionando herois", 5)
-
-    click_button(864, 374, "colocando todos herois para trabalhar", 5)
-
-    click_button(1005, 323, "fechando janela de herois", 5)
-
-    click_button(943, 502, "começando a mineração", 5)
-
-
-print("Deixe o navegador aberto no site do bombcrypto com a metamask logada")
-
+    click_button(943, 502, 'hunt', 5)
 
 while True:
-    click_button(150, 250, "Selecionando o navegador", 1)
-    refresh_page()
+    print('deixe o navegador aberto no site do bombcrypto')
+    time.sleep(2)
+    click_button(250, 150, 'selecting windows', 1)
+
+    pyautogui.hotkey('ctrl', 'r')
+    print('aguardando 20 segundos para começar')
+    time.sleep(20)
+    
+    print('starting...')
     rotine()
     print(datetime.datetime.today())
     time.sleep(time_work)
